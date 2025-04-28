019c9eaff0ab4b26a6681a3cbc40aa2a0faa9dd5
mehak2299 <kaurkavneet72@gmail.com>
2025-04-28 15:07:31 +0530

    Choice

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ textarea.focus()
 
 textarea.addEventListener('keyup',(e)=>{
 createTags(e.target.value)
+if(e.key==='Enter')
+{
+    setTimeout(() => {
+    e.target.value=''
+    }, 10);
+    randomSelect()
+}
 })
 
 const createTags=(input)=>{
@@ -20,3 +27,42 @@ tags.forEach(tag=>{
  tagsEl.appendChild(tagEl)
 })
 }
+const randomSelect=()=>{
+    const times=10;
+    const interval=setInterval(() => {
+        const randomTag=pickRandomTag()
+        if(randomSelect)
+        {
+        highlightTag(randomTag)
+        setTimeout(()=>{
+            unHighlightTag(randomTag)
+        },100)
+        }
+        
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+const pickRandomTag=()=>{
+    const tags=document.querySelectorAll('.tag')
+   return tags[Math.floor(Math.random()*tags.length)]
+}
+
+const highlightTag=(tag)=>{
+tag.classList.add('higlight')
+}
+
+const unHighlightTag=(tag)=>{
+    tag.classList.remove('higlight')
+    }
+    
